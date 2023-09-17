dac8ab6078f67111f1cd2460f4d0e6705d07f978
Vedang chavan <vedang.chavan@iitgn.ac.in>
2023-09-17 20:09:42 +0530

    ngrams functions completed

--- n_gram.py
+++ n_gram.py
@@ -1,3 +1,6 @@
+from math import log,exp
+import numpy as np
+
 def vocabulary(train_corpus, test_corpus):
     vocab = set()
     
@@ -79,3 +82,68 @@ def train_n_gram(corpus, n, vocab = 0, smoothing = False):
             else:
                 prob_n_words[key] = (ngram_counts[key] + 1) / (N_train + vocab)
     return prob_n_words
+
+def test_n_gram(test_data, n, prob_words, epsilon=1e-15, Vocabulary=0,smoothing=False,processed_corpus=None):
+    perplexity={}
+    if(smoothing==False):
+        for id in test_data:
+            if(n==1):
+                tokens=["<s>"]
+            else: 
+                tokens=["<s>"]*(n-1)
+            tokens+=test_data[id]
+            if(n==1):
+                tokens+=["</s>"]
+            else:
+                tokens+=["</s>"]*(n-1)
+            data_len = len(tokens)
+            log_p=0
+            for i in range(0,data_len-n+1):
+                str=""
+                for j in range(i,i+n-1):
+                    str+=tokens[j]+" "
+                str+=tokens[i+n-1]
+                if str in prob_words:
+                    log_p += log(prob_words[str])
+                else:
+                    log_p += log(epsilon)
+            log_p=(-log_p)/data_len
+            perplexity[id]=exp(log_p)
+        
+        avg_perplexity=np.mean(list(perplexity.values()))
+
+    else:
+        n1gram_counts=count_n_gram(processed_corpus,n-1)
+        unigram_counts=count_n_gram(processed_corpus,1)
+
+        for id in test_data:
+            if(n==1):
+                tokens=["<s>"]
+            else: 
+                tokens=["<s>"]*(n-1)
+            tokens+=test_data[id]
+            if(n==1):
+                tokens+=["</s>"]
+            else:
+                tokens+=["</s>"]*(n-1)
+            data_len = len(tokens)
+            log_p=0
+            for i in range(0,data_len-n+1):
+                prev=""
+                for j in range(i,i+n-1):
+                    prev+=tokens[j]+" "
+                str=prev+tokens[i+n-1]
+                if str in prob_words:
+                    log_p += log(prob_words[str])
+                else:
+                    if(prev in n1gram_counts):
+                        log_p += log(1/n1gram_counts[prev]+Vocabulary)
+                    elif(n>1 and tokens[i+n-2]=="<s>"):
+                        log_p += log(1/unigram_counts["<s>"]+Vocabulary)
+                    else:
+                        log_p += log(1/Vocabulary)
+            log_p=(-log_p)/data_len
+            perplexity[id]=exp(log_p)
+        
+        avg_perplexity=np.mean(list(perplexity.values()))
+    return perplexity,avg_perplexity
